ca1a143efb212cc7f320eabb76403555ba565061
Soyoung Jang <al2440buss@gmail.com>
2025-02-01 21:03:51 +0900

    [Bronze III] Title: 학점계산, Time: 176 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,67 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		double result = 0;
+		
+		switch(str) {
+		case "A+":
+			result = 4.3;
+			break;
+		case "A0":
+			result = 4.0;
+			break;
+		case "A-":
+			result = 3.7;
+			break;
+			
+		case "B+":
+			result = 3.3;
+			break;
+		case "B0":
+			result = 3.0;
+			break;
+		case "B-":
+			result = 2.7;
+			break;
+			
+		case "C+":
+			result = 2.3;
+			break;
+		case "C0":
+			result = 2.0;
+			break;
+		case "C-":
+			result = 1.7;
+			break;
+			
+		case "D+":
+			result = 1.3;
+			break;
+		case "D0":
+			result = 1.0;
+			break;
+		case "D-":
+			result = 0.7;
+			break;
+			
+		case "F":
+			result = 0.0;
+			break;
+		default :
+			break;
+		}
+		
+		
+		System.out.println(result);
+		
+		sc.close();
+		
+	}
+
+}
